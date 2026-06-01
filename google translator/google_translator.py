cabf16bb994b4b5522435d3ae224a33f6c247749
pranav-ANIL <pranavsiuuuu@gmail.com>
2026-06-01 14:08:44 +0530

    Delete google translator/google_translator.py

--- google translator/google_translator.py
+++
@@ -1,5 +0,0 @@
-from googletrans import Translator
-translator = Translator()
-text = input("enter text in english:")
-translated=translator.translate(text,dest="ml")
-print("malayalam:",translated.text)
