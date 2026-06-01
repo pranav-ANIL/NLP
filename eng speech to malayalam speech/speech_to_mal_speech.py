35dc16408857d60b2b1d2b362c0efd849e1a9d07
pranav-ANIL <pranavsiuuuu@gmail.com>
2026-06-01 14:14:32 +0530

    Add files via upload

---
+++ eng speech to malayalam speech/speech_to_mal_speech.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+from googletrans import Translator
+from gtts import gTTS
+import os
+r = sr.Recognizer()
+translator = Translator()
+with sr.Microphone() as source:
+    print("please speak")
+    try:
+        audio = r.listen(source,timeout=7)
+        text = r.recognize_google(audio)
+        print("you said:",text)
+        translated=translator.translate(text,src="en",dest="ml")
+        print(translated.text)
+        tts = gTTS(text=translated.text, lang="ml")
+        tts.save("malayalam_output.mp3")
+        os.system("start malayalam_output.mp3")
+    except sr.WaitTimeoutError:
+        print("you did not speak.please speak")
+    except sr.UnknownValueError:
+        print("i heard something,but could not understand.please speak clearly")
